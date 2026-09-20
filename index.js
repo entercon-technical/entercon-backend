5e304dac9644d961a2fdb859625320114fc83523
entercon-technical <techentercon@gmail.com>
2026-09-20 17:42:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ let data;
 
 mongoose
   .connect(
-    "mongodb+srv://entercontech:Tech@11Entercon@cluster0.lpvcsca.mongodb.net/entercon?appName=Cluster0"
+    "mongodb+srv://entercontech:Tech%4011Entercon@cluster0.lpvcsca.mongodb.net/entercon?appName=Cluster0"
   )
   .then(() => {
     console.log("Connected to MongoDB");
